da9c67aa3a9a5d56664d92e5e409bac7da7a2778
German Popoter <g.popoteur@gmail.com>
2014-01-06 14:12:23 +0100

    running jshint to the plugin

--- src/jquery-creditcard.js
+++ src/jquery-creditcard.js
@@ -1,59 +1,63 @@
+/*global jQuery*/
+
 (function($) {
+	'use strict';
 
-var CreditCardPlugin = function (options) {
-	var self = this;
+	var CreditCardPlugin = function () {
+		var self = this;
 
-	self.validate = function () {
-		return $.CreditCard.validateCard(self.val());
+		self.validate = function () {
+			return $.CreditCard.validateCard(self.val());
+		};
+
+		return self;
+	};
+
+	if(!$.CreditCard){
+		$.CreditCard = {};
 	}
 
-	return self;
-}
+	// API
+	$.CreditCard.validateCard = function (str) {
+		// Remove dashes (if any)
+		var card = str.replace(/-/g, ''),
+			total = 0,
+			num = 0;
 
-if(!$.CreditCard){
-	$.CreditCard = {};
-}
+		// Double every other digit to the right
+		for (var i = 0; i < card.length; i += 2) {
+			num = Number(card[i]) * 2;
 
-// API
-$.CreditCard.validateCard = function (str) {
-	// Remove dashes (if any)
-	var card = str.replace(/-/g, ''),
-		total = 0;
+			// Add the numbers to a total Sum.
 
-	// Double every other digit to the right
-	for (var i = 0; i < card.length; i += 2) {
-		var num = Number(card[i]) * 2;
+			// If the number has multiple digits,
+			// the digits need to be added seperately
+			if(num > 9){
+				var numStr = num.toString();
+				for (var j = 0; j < numStr.length; j++) {
+					total += Number(numStr.charAt(j));
+				}
+			}else{
+				total += num;
+			}
+		}
 
-		// Add the numbers to a total Sum.
-
-		// If the number has multiple digits,
-		// the digits need to be added seperately
-		if(num > 9){
-			var numStr = num.toString();
-			for (var j = 0; j < numStr.length; j++) {
-				total += Number(numStr.charAt(j));
-			};
-		}else{
+		// To the previous total add the rest of the
+		// digits on the card to the same total
+		for (var k = 1; k < card.length; k += 2) {
+			num = Number(card[k]);
 			total += num;
 		}
+
+		// If the total is divisible by 10
+		// the number is valid
+		return (total % 10 === 0);
 	};
 
-	// To the previous total add the rest of the
-	// digits on the card to the same total
-	for (var k = 1; k < card.length; k += 2) {
-		var num = Number(card[k]);
-		total += num;
+	// jQuery Definition of the plugin
+	$.fn.CreditCard = function () {
+		return CreditCardPlugin.apply(this, Array.prototype.slice.call(arguments, 1));
 	};
-
-	// If the total is divisible by 10
-	// the number is valid
-	return (total % 10 === 0);
-}
-
-// jQuery Definition of the plugin
-$.fn.CreditCard = function () {
-	return CreditCardPlugin.apply(this, Array.prototype.slice.call(arguments, 1));
-};
 
 })(jQuery);
 
