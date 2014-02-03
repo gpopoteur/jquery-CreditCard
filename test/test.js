5e4c5a2e537c2f39b2fd532d619d904bed6cc737
German Popoter <g.popoteur@gmail.com>
2014-02-03 22:47:06 +0100

    adding the first group of test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,14 @@ describe('jQuery Credit Card', function() {
 	describe('Plugin', function () {
 		it.skip('should check if the credit card number could be real',function () {
 			
-		})
+		});
+
+		it.skip('should identify the card type', function () {
+			
+		});
+
+		it.skip('should identify the major industry', function () {
+			
+		});
 	});
 });
